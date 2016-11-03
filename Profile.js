2a191c8b65bbe7846c5b63ed2d5429529b85b6c5
Zean Qin <me@zean.be>
2016-11-03 13:00:56 +1100

    Testing Profile

---
+++ Profile.js
@@ -0,0 +1,30 @@
+import {expect} from 'chai';
+import {Selector} from 'testcafe';
+
+fixture ('Profile')
+    .page('http://qa.clinicion.com')
+    .beforeEach( async t => {
+      await t
+      .typeText('#UserName', 'me@zean.be')
+      .typeText('#Password', 'qiN...4A')
+      .click('.btn-login');
+    });
+
+test('Updating Profile', async t => {
+  // make sure we are on the landing page
+  expect((await t.select('#Title')).value).to.equal('Assess Team');
+
+  // navigate to profile page and check the state
+  await t.click('.user-title a');
+  expect((await t.select('.action-bar-update i span')).innerText).to
+    .equal('Update');
+
+  // click on the update button and check the page state 
+  await t.click('.action-bar-update');
+  expect((await t.select('.post-action i span')).innerText).to.equal('Save');
+  expect((await t.select('#Title')).value).to.equal('Update Profile');
+
+  // save changes and check the page state
+  await t.click('.post-action i span');
+  expect((await t.select('.action-bar-update i span')).innerText).to.equal('Update');
+});
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # clinicion-automated-tests
 Automated browser tests for qa.clinicion.com
+
+To run the tests: 
+  1. Install testcafe: https://github.com/DevExpress/testcafe
+  2. Download the test files and run testcafe on them.
